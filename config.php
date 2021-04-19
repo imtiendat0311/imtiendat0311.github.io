c7b4c059e0c709d14d28b75627c2aef545fd11e3
imtiendat0311 <60676245+imtiendat0311@users.noreply.github.com>
2021-04-19 16:22:39 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,47 @@
+<?
+$config=array(
+
+);
+
+$wording=array(
+    "en"=>array(
+        "language"=>"English",
+        "head"=>"A Connection That Can Keep Up With You",
+        "pas"=>"Not a Marriott Bonvoy&trade; Member? <a href="">Join Now...it's Free</a>",
+        "int"=>"Internet",
+        "free"=>"Free For Everyone"
+        "btn_login"=>"CONNECT NOW",
+        "wait"=>"PLEASE WAIT",
+
+    ),
+    "vn"=>array(
+        "language"=>"Việt Nam",
+        "head"=>"Một Kết Nối Có Thể Theo Kịp Với Bạn",
+        "pas"=>"Không phải là thành viên của Marriot Bonvoy? <a href="">Tham gia ngay</a>",
+        "int"=>"Internet",
+        'free'=>"Miễn Phí Cho Mọi Người",
+        'btn_login'=>"KẾT NỐI NGAY",
+        "wait"=>"ĐANG KẾT NỐI",
+
+    ),
+    "jp"=>array(
+        "language"=>"日本語‬",
+        "head"=>"あなたに追いつくことができる接続",
+        "pas"=>"マリオットボンボイメンバーではありませんか？ <a href="">今すぐ参加...無料です</a>",
+        "int"=>"インターネット",
+        "free"=>"誰でも無料"
+        "btn_login"=>"今すぐ接続",
+        "wait"=>"お待ちください",
+    ),
+    "kr"=>array(
+        "language"=>"한국어‬",
+        "head"=>"당신을 따라갈 수있는 연결",
+        "pas"=>"Marriott Bonvoy&trade;회원이 아니십니까? <a href="">지금 가입하세요 ... 무료입니다</a>", 
+        "link"=>"지금 가입하세요 ... 무료입니다",
+        "int"=>"인터넷",
+        "free"=>"누구나 무료"
+        "btn_login"=>"지금 연결",
+        "wait"=>"기다려주세요",
+    ),
+);
+?>
